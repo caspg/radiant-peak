96d834bdb2a1442436c60581e565ef2e13483391
Kacper Golinski <kacper.golinski@gmail.com>
2016-09-15 12:03:43 +0200

    update displaying results

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def prettify_analyzer_result(username, result)
+    result[:errors].nil? ? result_message(username, result) : result[:errors]
+  end
+
+  def result_message(username, result)
+    "#{username}'s favourite language is #{result[:favourite_language]}"
+  end
 end
