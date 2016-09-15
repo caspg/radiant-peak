9190058d863b1ddd1caa00a792b92b2de458882d
Kacper Golinski <kacper.golinski@gmail.com>
2016-09-15 11:44:24 +0200

    call repos_analyzer in home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @analyzer_result = github_username.present? ? analyzer_result : nil
+  end
+
+  private
+
+  def analyzer_result
+    Github::ReposAnalyzer.new(github_username).find_favourite_language
+  end
+
+  def github_username
+    params[:github_username]
   end
 end
